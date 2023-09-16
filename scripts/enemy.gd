e973d9de56023d7da71db1071e721cd4af41dd52
viniromao <vinicius.santos@picpay.com>
2023-09-16 09:34:10 -0300

    no message

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,17 +3,15 @@ class_name Enemy extends Area2D
 @export var speed = 20
 
 func _physics_process(delta):
-		global_position.x += -speed * delta
-		
+	global_position.x += -speed * delta
+
 func die():
 	queue_free()
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
