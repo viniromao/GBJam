98655b5fbddcf1d39cfe1ff6fa1fd5af4d41216b
edugaRP <edulindorodrigues@gmail.com>
2023-09-16 09:58:26 -0300

    bla

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -6,11 +6,12 @@ extends Area2D
 func _physics_process(delta):
 	global_position.x += speed * delta
 
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	queue_free()
-
-
 func _on_area_entered(area):
 	if area is Enemy:
 		area.die()
 		queue_free()
+
+
+
+func _on_visible_on_screen_enabler_2d_screen_exited():
+	queue_free()
