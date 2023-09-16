856face46ada91f73ee763a4f5d693f18bbebcb8
edugaRP <edulindorodrigues@gmail.com>
2023-09-16 15:00:33 -0300

    main menu, game over and intro scenes created

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var spawn_player_pos = $PlayerSpawnPos
 @onready var laser_container = $LaserContainer
+@onready var gos = $UILayer/GameOver
 
 var player = null
 
@@ -10,6 +11,7 @@ func _ready():
 	assert(player != null)
 	player.global_position = spawn_player_pos.global_position
 	player.laser_shot.connect(_on_player_laser_shot)
+	player.killed.connect(_on_player_killed)
 
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
@@ -21,3 +23,7 @@ func _on_player_laser_shot(laser_scene, location):
 	var laser = laser_scene.instantiate()
 	laser.global_position = location
 	laser_container.add_child(laser)
+	
+func _on_player_killed():
+	await get_tree().create_timer(0.5).timeout
+	gos.visible = true
