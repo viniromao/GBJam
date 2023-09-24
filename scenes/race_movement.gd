71638f6160562fb54ea12b82e4d55ab3ba3321ec
viniromao <vinicius.santos@picpay.com>
2023-09-24 20:28:12 -0300

    merge

--- scenes/race_movement.gd
+++ scenes/race_movement.gd
@@ -5,6 +5,8 @@ var acceleration = 5
 var final_speed = 250
 
 func _process(delta):
+	if(position.x <= -31900):
+		get_tree().change_scene_to_file("res://scenes/level3.tscn")
 	self.position.x -= speed * delta
 	
 	if final_speed != speed:
